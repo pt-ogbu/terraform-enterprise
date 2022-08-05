cb840aa0e935f8e1bcc3d6b326ac3c87fe41d4e1
pt-ogbu <99296599+pt-ogbu@users.noreply.github.com>
2022-08-04 20:05:49 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
  terraform {
    backend "s3" {
-    bucket = "code-sharing-bucket"
-    key    = "avinash/s3file-3.tfstate"
+    bucket = "terraform-enterprise-chukwu1"
+    key    = "terraform-enterprise-chukwu1.tfstate"
     region = "us-west-2"
-    profile = "jespo"
+    profile = "us-west2"
 dynamodb_table = "firstprojecttable"
    }
  }
